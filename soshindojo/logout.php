ebebf37c0abc2a7bdf5772c3c00b439259536227
armstrongdev-lab <60799940+armstrongdev-lab@users.noreply.github.com>
2026-04-29 14:38:26 -0300

    Delete soshindojo directory

--- soshindojo/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-session_unset(); // limpa todas as variáveis de sessão
-session_destroy(); // destrói a sessão
-
-header('Location: index.php');
- // ou o caminho correto da sua tela de login
-exit;
-?>
